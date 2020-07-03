d82350ef280ce8a14dd235c8997bb2a4bd6fab72
AZcoder24 <67431079+AZcoder24@users.noreply.github.com>
2020-07-03 17:06:57 +0300

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,12 @@
+print("Calculator")
+num_1=float(input("Enter First Number:"))
+op_type=input("Enter Operation:")
+num_2=float(input("Enter Second Number:"))
+if op_type == "+":
+    print(num_1+num_2)
+if op_type == "-":
+    print(num_1-num_2)
+if op_type == "*":
+    print(num_1*num_2)
+if op_type == "/":
+    print(num_1/num_2)
